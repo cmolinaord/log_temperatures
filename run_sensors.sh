21566390c1b939e5dc37becf69ddb34a5161f1da
cmolinaord <carlosmolina.ord@gmail.com>
2023-04-03 16:03:30 +0200

    First script to run all sensors

---
+++ run_sensors.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+python /proyectos/scripts/temperatura_casa/log_temperatures/log_temp.py salon
+python /proyectos/scripts/temperatura_casa/log_temperatures/log_temp.py balcon
+python /proyectos/scripts/temperatura_casa/log_temperatures/log_temp.py cuarto
